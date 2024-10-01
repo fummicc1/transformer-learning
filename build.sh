75d127cc26b5aa3dd7dee938ba62e0cad4d7aa23
fummicc1 <fumiya.tennis1@gmail.com>
2024-10-02 00:47:57 +0900

    fix: check build and run

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 
 mkdir -p build
 cd build
-cmake -DCMAKE_PREFIX_PATH=/home/fummicc1/codes/Examples/transformer-learning/libtorch ..
+libtorch_path=$(pwd)/../libtorch
+# absolute path to libtorch
+libtorch_path=$(realpath ../libtorch)
+echo "libtorch_path: $libtorch_path"
+cmake -DCMAKE_PREFIX_PATH=$libtorch_path ..
+cmake --build . --config Release
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+wget https://download.pytorch.org/libtorch/cu121/libtorch-cxx11-abi-shared-with-deps-2.4.1%2Bcu121.zip
+unzip libtorch-cxx11-abi-shared-with-deps-2.4.1%2Bcu121.zip
